61564ef73e26a478e768989188aa604d7bd8d3c7
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2021-11-08 01:07:22 +0300

    draggable

---
+++ draggable-resizable-component/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {AngularDraggableModule} from "angular2-draggable";
+import {StimulsoftViewerModule} from "stimulsoft-viewer-angular";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularDraggableModule,
+    BrowserModule,
+    StimulsoftViewerModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
