6960206c5701d5fc05caf8a0b5929b4e97fbfd4b
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2022-06-02 23:23:32 +0300

    need to modify dragdrop library

--- draggable-resizable-component/src/app/app.module.ts
+++ draggable-resizable-component/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AngularDraggableModule,
     BrowserModule,
     StimulsoftViewerModule,
