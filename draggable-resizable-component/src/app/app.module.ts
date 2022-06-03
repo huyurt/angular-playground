7647effbc91f77bfb48b74c3dc5bbd82d08b449f
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2022-06-04 01:53:38 +0300

    modified dragdrop library

--- draggable-resizable-component/src/app/app.module.ts
+++ draggable-resizable-component/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {AngularDraggableModule} from "angular2-draggable";
+import {AngularDraggableModule} from "angular2-draggable-zi";
 import {StimulsoftViewerModule} from "stimulsoft-viewer-angular";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpClientModule} from "@angular/common/http";
